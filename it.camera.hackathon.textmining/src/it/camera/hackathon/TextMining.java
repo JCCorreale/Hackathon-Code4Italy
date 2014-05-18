a044d8631133e4178a5c0a5fc932beee1781fb30
JCCorreale <jeanclaudecorreale@gmail.com>
2014-05-18 04:07:05 +0200

    Added some stopwords.

--- it.camera.hackathon.textmining/src/it/camera/hackathon/TextMining.java
+++ it.camera.hackathon.textmining/src/it/camera/hackathon/TextMining.java
@@ -15,8 +15,8 @@ import it.camera.hackathon.textmining.scraping.SynonimScraper;
 
 public class TextMining {
 
-	private static int topWordsCount = 10;
-	private static int minWordLength = 6;
+	private static int topWordsCount = 20;
+	private static int minWordLength = 3;
 	private static String delimiters = " '";
 	
 	public static void main(String[] args)
@@ -30,6 +30,8 @@ public class TextMining {
 		System.out.println("Removing HTML tags...");
 		String plainText = HtmlRemover.text(html);
 		
+		// TODO Stemmer?
+		
 		// counts the occurence
 		System.out.println("Counting word occurences (may take a while!)...");
 		TextMiningWordCounter counter = buildWordCounter();
@@ -41,11 +43,11 @@ public class TextMining {
 		List<Entry<String, Integer>> topWords = analyzer.getSortedWords(topWordsCount, result);
 
 		// prints synonyms for the top words
-//		SynonimScraper synonimScraper = new SynonimScraper();
-//		for (Entry<String, Integer> entry : topWords)
-//		{
-//			System.out.println(entry.getKey() + " " + entry.getValue() + synonimScraper.FindSynonims(entry.getKey()));
-//		}
+		SynonimScraper synonimScraper = new SynonimScraper();
+		for (Entry<String, Integer> entry : topWords)
+		{
+			System.out.println(entry.getKey() + " " + entry.getValue() + synonimScraper.FindSynonims(entry.getKey()));
+		}
 		
 		// creates an IDocument instance from the retrieved data
 		IDocument document = buildDocument(topWords, counter.getAcceptedWordCount());
@@ -54,11 +56,18 @@ public class TextMining {
 		//Utils.printWordEntries(topWords);
 	}
 	
+	private static String[] getStopWords()
+	{
+		return ("a abbia abbiamo abbiano abbiate ad adesso agl agli ai al all alla alle allo allora altre altri altro anche ancora avemmo avendo avere avesse avessero avessi avessimo aveste avesti avete aveva avevamo avevano avevate avevi avevo avrai avranno avrebbe avrebbero avrei avremmo avremo avreste avresti avrete avrà avrò avuta avute avuti avuto ben buono c che chi ci cinque coi col come comprare con consecutivi consecutivo contro cosa cui da dagl dagli dai dal dall dalla dalle dallo degl degli dei del dell della delle dello dentro deve devo di doppio dov dove due e ebbe ebbero ebbi ecco ed era erano eravamo eravate eri ero essendo faccia facciamo facciano facciate faccio facemmo facendo facesse facessero facessi facessimo faceste facesti faceva facevamo facevano facevate facevi facevo fai fanno farai faranno fare farebbe farebbero farei faremmo faremo fareste faresti farete farà farò fece fecero feci fine fino fosse fossero fossi fossimo foste fosti fra fu fui fummo furono gente giu gli ha hai hanno ho i il in indietro invece io l la lavoro le lei li lo loro lui lungo ma me meglio mi mia mie miei mio molta molti molto ne negl negli nei nel nell nella nelle nello no noi nome non nostra nostre nostri nostro nove nuovi nuovo o oltre ora otto peggio per perché pero persone piu più poco primo promesso qua quale quanta quante quanti quanto quarto quasi quattro quella quelle quelli quello questa queste questi questo qui quindi quinto rispetto sara sarai saranno sarebbe sarebbero sarei saremmo saremo sareste saresti sarete sarà sarò se secondo sei sembra sembrava senza sette si sia siamo siano siate siete solo sono sopra soprattutto sotto sta stai stando stanno starai staranno starebbe starebbero starei staremmo staremo stareste staresti starete starà starò stati stato stava stavamo stavano stavate stavi stavo stemmo stesse stessero stessi stessimo stesso steste stesti stette stettero stetti stia stiamo stiano stiate sto su sua subito sue sugl sugli sui sul sull sulla sulle sullo suo suoi tanto te tempo terzo ti tra tre triplo tu tua tue tuo tuoi tutti tutto ultimo un una uno va vai vi voi volte vostra vostre vostri vostro"
+				+ " comma commi articolo articoli legge leggi anno anni decreto decreti entro enti ente norma norme codice codici pagina pagine pag pag. decreto-legge decreti-legge disposizioni legislativo legislativa legislatura parte parti ministero ministeri presente presenti senso sensi prevedere prevede prevedono essere sono stato stati").split(" ");
+	}
+	
 	private static TextMiningWordCounter buildWordCounter()
 	{
 		TextMiningWordCounter wordCounter = new TextMiningWordCounter();
 		wordCounter.setMinLength(minWordLength);
 		wordCounter.setDelimiters(delimiters);
+		wordCounter.setStopWords(getStopWords());
 		return wordCounter;
 	}
 	
@@ -70,8 +79,7 @@ public class TextMining {
 	private static List<Entry<ITerm, Integer>> getTerms(List<Entry<String, Integer>> topWords)
 	{
 		List<Entry<ITerm, Integer>> terms = new ArrayList<Entry<ITerm, Integer>>();
-		
-		
+		// TODO
 		return terms;
 	}
 	
