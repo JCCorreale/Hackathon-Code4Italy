d55097ff735235c521cf30256d7f1681043267b7
JCCorreale <jeanclaudecorreale@gmail.com>
2014-05-18 14:40:50 +0200

    Commit.

--- it.camera.hackathon.textmining/src/it/camera/hackathon/DocumentAnalyser.java
+++ it.camera.hackathon.textmining/src/it/camera/hackathon/DocumentAnalyser.java
@@ -1,19 +1,25 @@
 package it.camera.hackathon;
 
+import it.camera.hackathon.textmining.clustering.DocumentCollection;
 import it.camera.hackathon.textmining.clustering.IDocument;
 import it.camera.hackathon.textmining.clustering.IDocumentCollection;
 import it.camera.hackathon.textmining.clustering.ITerm;
 import it.camera.opendata.model.Atto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class DocumentAnalyser implements IDocumentAnalyser {
 
-	private static IDocumentCollection buildDocumentCollection()
+	private static IDocumentCollection buildDocumentCollection(Iterable<IDocument> args)
 	{
-		// TODO
-		return null;
+		List<IDocument> docs = new ArrayList<IDocument>();
+		for (IDocument doc : args)
+		{
+			docs.add(doc);
+		}
+		return new DocumentCollection(docs.toArray(new IDocument[0]));
 	}
 	
 	public DocumentAnalyser() 
