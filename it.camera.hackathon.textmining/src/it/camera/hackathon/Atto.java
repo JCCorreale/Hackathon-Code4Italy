cfe40c560412b98d4315ae036d9d21b338e01876
Thierry Spetebroot <thierry.spetebroot@gmail.com>
2014-05-23 09:31:53 +0200

    Atto - updated in a Bean like object (Atto represents an ActProposal)

--- it.camera.hackathon.textmining/src/it/camera/hackathon/Atto.java
+++ it.camera.hackathon.textmining/src/it/camera/hackathon/Atto.java
@@ -1,12 +1,54 @@
 package it.camera.hackathon;
 
+import java.util.Date;
+
+/**
+ * Proposed Act.
+ */
 public class Atto 
 {
 	private String iri;
 	
+	private String 	name;
+	private int 	legislature;
+	private Date 	revision;
+	private String 	contentIrl;
+	
 	public String getIRI() 
 	{
 		return iri;
+	}	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLegislature() {
+		return legislature;
+	}
+
+	public void setLegislature(int legislature) {
+		this.legislature = legislature;
+	}
+
+	public Date getRevision() {
+		return revision;
+	}
+
+	public void setRevision(Date revision) {
+		this.revision = revision;
+	}
+
+	public String getContentUrl() {
+		return contentIrl;
+	}
+
+	public void setContentUrl(String contentUrl) {
+		this.contentIrl = contentUrl;
 	}
 	
 	public Atto(String iri) 
